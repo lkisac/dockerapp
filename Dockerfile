20957417521b256b39f8a53b03c8c8bb98537626
lkisac01 <lenkei.isac@gmail.com>
2018-02-02 14:24:09 -0500

    use redis get/set apis in python script and install redis in Dockerfile

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,7 @@
 # base image
 FROM python:3.5
-# install Flask
-RUN pip install Flask==0.11.1
+# install Flask and redis
+RUN pip install Flask==0.11.1 redis==2.10.5
 # add new user 'admin'.  -ms option creates default home directory
 RUN useradd -ms /bin/bash admin
 # run app under 'admin' user, otherwise will run as root
--- app/app.py
+++ app/app.py
@@ -1,8 +1,10 @@
 from flask import Flask, request, render_template
+import redis
 
 app = Flask(__name__)
 default_key = '1'
-cache = {default_key: 'one'} 
+cache = redis.StrictRedis(host='redis', port=6379, db=0) # 'redis' is container name
+cache.set(default_key, "one") # use redis set api
 
 @app.route('/', methods=['GET', 'POST'])
 def mainpage():
@@ -12,12 +14,12 @@ def mainpage():
 	    key = request.form['key']
 
 	if request.method == 'POST' and request.form['submit'] == 'save':
-		cache[key] = request.form['cache_value']
+		cache.set(key, request.form['cache_value'])
 
 	cache_value = None;
         # key value lookup
-	if key in cache:
-		cache_value = cache[key]
+        if cache.get(key):
+		cache_value = cache.get(key).decode('utf-8') # use redis get api
 
 	return render_template('index.html', key=key, cache_value=cache_value)
 
