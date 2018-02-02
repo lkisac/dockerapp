6ae7e9290209a0d2e7babca44da6665eff4bddc3
lkisac01 <lenkei.isac@gmail.com>
2018-02-02 15:57:07 -0500

    cleaned up code to fix docker build error

--- app/app.py
+++ app/app.py
@@ -3,25 +3,27 @@ import redis
 
 app = Flask(__name__)
 default_key = '1'
-cache = redis.StrictRedis(host='redis', port=6379, db=0) # 'redis' is container name
-cache.set(default_key, "one") # use redis set api
+# 'redis' is container name
+cache = redis.StrictRedis(host='redis', port=6379, db=0)
+# use redis set api
+cache.set(default_key, "one")
 
 @app.route('/', methods=['GET', 'POST'])
 def mainpage():
 
-	key = default_key
-	if 'key' in request.form:
-	    key = request.form['key']
+    key = default_key
+    if 'key' in request.form:
+        key = request.form['key']
 
-	if request.method == 'POST' and request.form['submit'] == 'save':
-		cache.set(key, request.form['cache_value'])
+    if request.method == 'POST' and request.form['submit'] == 'save':
+        cache.set(key, request.form['cache_value'])
 
-	cache_value = None;
-        # key value lookup
-        if cache.get(key):
-		cache_value = cache.get(key).decode('utf-8') # use redis get api
+    cache_value = None
+    # key value lookup - use redis get api
+    if cache.get(key):
+        cache_value = cache.get(key).decode('utf-8')
 
-	return render_template('index.html', key=key, cache_value=cache_value)
+    return render_template('index.html', key=key, cache_value=cache_value)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
