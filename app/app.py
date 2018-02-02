a9e47571e9a91adcaef24ca3d75f29f8b04c731a
lkisac01 <lenkei.isac@gmail.com>
2018-02-02 10:45:27 -0500

    added comments to Dockerfile and python script

--- app/app.py
+++ app/app.py
@@ -1,9 +1,15 @@
-from flask import Flask
+# initialize and start python server
+
+# import Flask module
+from flask import Flask 
 app = Flask(__name__)
 
+# register view function for given URL /
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
 
+# initializes python flask server
+# note: do not use this run method in production, use UWSGI or CGI instead
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
